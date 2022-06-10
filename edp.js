3482d141a602ab994b13a74dc1880b7662911b87
Gustavo Canavate <gustavocanavate@gmail.com>
2022-06-09 21:26:07 -0300

    main

---
+++ edp.js
@@ -0,0 +1,27 @@
+
+let litros = document.createElement ('h2');
+litros.innerHTML='<h2>',consumo'</h2>';
+let divLitros = document.getElementById(combus);
+consumo = toString(consumo);
+divLitros.append(consumo);
+
+let emision = document.createElement ('h2');
+emision.innerHTML='<h2>',emision'</h2>';
+let divEmision = document.getElementById(emision);
+emision = toString(emision);
+divLitros.append(emision);
+
+let informe = document.createElement ('h2');
+informe.innerHTML='<h2>',divInforme'</h2>';
+let divInforme = document.getElementById(informe);
+divInforme.append('Emisiones del vehículo ',tc,' para el período');
+
+
+
+
+
+
+
+
+
+
