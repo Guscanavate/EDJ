8b3c2b300b9bd580634ebc6f699bfb34f0365d5d
Gustavo Canavate <gustavocanavate@gmail.com>
2022-07-19 15:42:22 -0300

    final3

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    document.getElementById('body').classList.toggle('dark');
+    document.getElementById('body').classList.toggle(remove);
+}
+
+let noche = document.getElementById('toggle-btn-1');
+
+noche.addEventListener("Onclick") (changeColor);
+
+
+
+
+
+
